de3da2cd19d0495beec963f0d30fcb2e97d13236
kava <wabutrune.p@hotmail.com>
2022-09-06 10:03:24 +0200

    fermeture du formulaire

--- index.php
+++ index.php
@@ -1,18 +1,40 @@
 <?php
 
 class Form{
-    // 3 initialiser les propriétés nécessaires
-    private $method;
+
+    private $form;
+    // 3 initialiser les propriétés nécessaires si besoin d'un get
+    // private $method;
     // 2 rajouter les attribue de ma fonction si besoin
     public function __construct($method)
     {
-        // configurer les propriétés
-        $this->method = $method;
+        // configurer les propriétés si besoin d'un get
+        // $this->method = $method;
 
         //1  créer les opérations de la fonction
-        echo "<form method = ' ".$method . "'><fieldset>";
+        // echo "<form method = ' ".$method . "'><fieldset>";
 
 
+        
+        $this -> form = "<form method = ' ".$method . "'><fieldset>";
+
+    }
+    // Ajouter une zone texte
+    public function setText($name){
+        // echo "<input type = 'text' name = '".$name ."'>";
+
+        $this -> form =  $this -> form . "<input type = 'text' name = '".$name ."'>";
+    }
+
+    public function setSubmit(){
+        // echo "<bouton type = 'submit' value = 'Envoyer'>";
+
+        $this -> form .= "<bouton type = 'submit' value = 'Envoyer'>";
+    }
+    
+    public function getForm(){
+        return $this -> form;
     }
 
 }
+
